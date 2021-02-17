a706d5cbe6ef40ec98cbb642a4520fa4be7d66c7
dinidu_hewage <mailtodinidu@gmail.com>
2021-02-17 23:27:08 +0530

    converting_java_object_types_json_arrays

--- src/main/java/lk/gov/health/procedureroomservice/service/ProcedureRoomFacadeREST.java
+++ src/main/java/lk/gov/health/procedureroomservice/service/ProcedureRoomFacadeREST.java
@@ -19,6 +19,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import lk.gov.health.procedureroomservice.ProcedureRoom;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 /**
@@ -59,22 +60,37 @@ public class ProcedureRoomFacadeREST extends AbstractFacade<ProcedureRoom> {
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public ProcedureRoom find(@PathParam("id") Long id) {
-        return super.find(id);
+    public String find(@PathParam("id") Long id) {
+        return getJSONObject(super.find(id)).toString();
     }
 
     @GET
-    @Override
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<ProcedureRoom> findAll() {
-        return super.findAll();
+    public String getAll() {
+        JSONArray ja_ = new JSONArray();
+
+        List<ProcedureRoom> procRoomList;
+        procRoomList = super.findAll();
+
+        for (ProcedureRoom procRoom : procRoomList) {
+            ja_.add(getJSONObject(procRoom));
+        }
+        return ja_.toString(); 
     }
 
     @GET
     @Path("{from}/{to}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<ProcedureRoom> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
+    public String findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        JSONArray ja_ = new JSONArray();
+
+        List<ProcedureRoom> procRoomList;
+        procRoomList = super.findRange(new int[]{from, to});
+
+        for (ProcedureRoom procRoom : procRoomList) {
+            ja_.add(getJSONObject(procRoom));
+        }
+        return ja_.toString();
     }
 
     @GET
@@ -82,7 +98,7 @@ public class ProcedureRoomFacadeREST extends AbstractFacade<ProcedureRoom> {
     @Produces(MediaType.APPLICATION_JSON)
     public String countREST() {
         JSONObject jo = new JSONObject();
-        jo.put("room_count", String.valueOf(super.count()));
+        jo.put("count", String.valueOf(super.count()));
         return jo.toJSONString();
     }
 
@@ -90,5 +106,17 @@ public class ProcedureRoomFacadeREST extends AbstractFacade<ProcedureRoom> {
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
+    private JSONObject getJSONObject(ProcedureRoom procRoom) {
+        JSONObject jo_ = new JSONObject();
+
+        jo_.put("id", procRoom.getId());
+        jo_.put("roomId", procRoom.getRoomId());
+        jo_.put("description", procRoom.getDescription());
+        jo_.put("type", procRoom.getType());
+        jo_.put("instituteId", procRoom.getInstituteId());
+        jo_.put("status", procRoom.getStatus());
+
+        return jo_;
+    }
 }
